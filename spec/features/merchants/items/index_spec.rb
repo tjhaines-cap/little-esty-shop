ef2910c1b25c55d58b6d844910b2805d4726d27a
Cory Bethune <corybbethune@gmail.com>
2022-06-05 21:01:40 -0700

    Test creating in progress

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -6,23 +6,12 @@ RSpec.describe 'merchant items index page' do
     @merch_2 = Merchant.create!(name: "One-Legs Fashion")
 
     @item_1 = @merch_1.items.create!(name: "Two-Leg Pantaloons", description: "pants built for people with two legs", unit_price: 5000)
-    @item_2 = @merch_1.items.create!(name: "Two-Leg Shorts", description: "shorts built for people with two legs", unit_price: 6000)
-    @item_3 = @merch_1.items.create!(name: "Hat", description: "hat built for people with two legs and one head", unit_price: 7000)
-    @item_4 = @merch_1.items.create!(name: "Double Legged Pant", description: "pants built for people with two legs", unit_price: 8000)
-    @item_5 = @merch_1.items.create!(name: "Stainless Steel, 5-Pocket Jean", description: "Shorts of Steel", unit_price: 9000)
-    @item_6 = @merch_1.items.create!(name: "String of Numbers", description: "54921752964273", unit_price: 10000)
+    @item_2 = @merch_1.items.create!(name: "Two-Leg Shorts", description: "shorts built for people with two legs", unit_price: 3000)
+    @item_3 = @merch_1.items.create!(name: "Hat", description: "hat built for people with two legs and one head", unit_price: 6000)
+    @item_4 = @merch_1.items.create!(name: "Double Legged Pant", description: "pants built for people with two legs", unit_price: 50000)
+    @item_5 = @merch_1.items.create!(name: "Stainless Steel, 5-Pocket Jean", description: "Shorts of Steel", unit_price: 3000000)
+    @item_6 = @merch_1.items.create!(name: "String of Numbers", description: "54921752964273", unit_price: 100)
     @item_7 = @merch_2.items.create!(name: "Pirate Pants", description: "Peg legs don't need pant legs", unit_price: 1000)
-
-    @cust_1 = Customer.create!(first_name: "Debbie", last_name: "Twolegs")
-
-    @invoice_1 = @cust_1.invoices.create!(status: 2)
-
-    @ii_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_1.unit_price, status: 0)
-    @ii_2 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_2.unit_price, status: 0)
-    @ii_3 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_3.unit_price, status: 0)
-    @ii_4 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_4.unit_price, status: 0)
-    @ii_5 = InvoiceItem.create!(item_id: @item_5.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_5.unit_price, status: 2)
-    @ii_6 = InvoiceItem.create!(item_id: @item_6.id, invoice_id: @invoice_1.id, quantity: 1, unit_price: @item_6.unit_price, status: 2)
 
   end
 
@@ -59,23 +48,13 @@ RSpec.describe 'merchant items index page' do
     visit "/merchants/#{@merch_1.id}/items"
     click_link("New Item")
     expect(current_path).to eq("/merchants/#{@merch_1.id}/items/new")
-
   end
 
-  it "lists the 5 most popular items by revenue" do
+  it "displays 5 most popular items and their best selling date" do
     visit "/merchants/#{@merch_1.id}/items"
-
-    within "#top_5_items" do
-      expect(@item_6.name).to appear_before(@item_5.name)
-      expect(@item_5.name).to appear_before(@item_4.name)
-      expect(@item_4.name).to appear_before(@item_3.name)
-      expect(page).to_not have_content(@item_1.name)
-
-      expect(page).to have_content("Total Revenue: $100.00")
-      expect(page).to have_content("Total Revenue: $90.00")
-      expect(page).to have_content("Total Revenue: $80.00")
-      expect(page).to have_content("Total Revenue: $70.00")
-      expect(page).to have_content("Total Revenue: $60.00")
-    end
+      within "favorite" do
+        expect(page).to have_content("Top 5 Most Popular Items")
+        expect(page).to have_content("Top selling date for #{@item_1.name} was #{@item_1.created_at}")
+      end
   end
 end
