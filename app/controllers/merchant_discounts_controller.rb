95aa3993905f2d8840eac05afd28c5dbadf7d764
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-10 16:31:10 -0600

    Feat: display discount percentage and quantity threshold

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -1,6 +1,7 @@
 class MerchantDiscountsController < ApplicationController
 
   def index
+    @discounts = Merchant.find(params[:merchant_id]).bulk_discounts
   end
   
 end
--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -9,21 +9,20 @@ RSpec.describe 'merchant discounts index page' do
     @discount_1 = @merch_1.bulk_discounts.create!(percentage: 20, quantity: 10)
     @discount_2 = @merch_1.bulk_discounts.create!(percentage: 30, quantity: 15)
     @discount_2 = @merch_2.bulk_discounts.create!(percentage: 15, quantity: 5)
- 
   end
 
   it 'displays all bulk discounts for merchant including link to show page' do
     visit "/merchants/#{@merch_1.id}/discounts"
-
+save_and_open_page
     within "#discount-0" do
-      expect(page).to have_content("Discount: 20%")
-      expect(page).to have_content("Quanitity Threshold: 10")
+      expect(page).to have_content("Discount 1: 20%")
+      expect(page).to have_content("Quantity Threshold: 10")
       click_link("Discount 1")
       expect(current_path).to eq("/merchants/#{@merch_1.id}/discounts/#{@discount_1.id}")
       visit "/merchants/#{@merch_1.id}/discounts"
     end
     within "#discount-1" do
-      expect(page).to have_content("Discount: 30%")
+      expect(page).to have_content("Discount 2: 30%")
       expect(page).to have_content("Quanitity Threshold: 15")
       click_link("Discount 2")
       expect(current_path).to eq("/merchants/#{@merch_1.id}/discounts/#{@discount_2.id}")
