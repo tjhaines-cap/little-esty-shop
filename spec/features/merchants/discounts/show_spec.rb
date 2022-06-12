6eef126ae87434161b94bc794e49e85f653c26e2
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-12 10:20:39 -0600

    Test: edit link

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'discount show page', type: :feature do
 
-  it 'displays discount percentage and quantity threshold' do
-    merch_1 = Merchant.create(name: "Schroeder-Jerde" )
-    merch_2 = Merchant.create(name: "Klein, Rempel and Jones")
+  before :each do
+    @merch_1 = Merchant.create(name: "Schroeder-Jerde" )
+    @merch_2 = Merchant.create(name: "Klein, Rempel and Jones")
 
-    discount_1 = merch_1.bulk_discounts.create!(percentage: 20, quantity: 10)
-    discount_2 = merch_1.bulk_discounts.create!(percentage: 30, quantity: 15)
-    discount_3 = merch_2.bulk_discounts.create!(percentage: 15, quantity: 5)
- 
-    visit "/merchants/#{merch_1.id}/discounts/#{discount_1.id}"
+    @discount_1 = @merch_1.bulk_discounts.create!(percentage: 20, quantity: 10)
+    @discount_2 = @merch_1.bulk_discounts.create!(percentage: 30, quantity: 15)
+    @discount_3 = @merch_2.bulk_discounts.create!(percentage: 15, quantity: 5)
+  end
+
+  it 'displays discount percentage and quantity threshold' do
+    
+    visit "/merchants/#{@merch_1.id}/discounts/#{@discount_1.id}"
 
     expect(page).to have_content("Percentage: 20%")
     expect(page).to have_content("Quantity Threshold: 10")
@@ -19,4 +22,13 @@ RSpec.describe 'discount show page', type: :feature do
     expect(page).to_not have_content("Percentage: 15%")
     expect(page).to_not have_content("Quantity Threshold: 5")
   end
+
+  it 'has a link to edit the discount' do
+    visit "/merchants/#{@merch_1.id}/discounts/#{@discount_1.id}"
+
+    click_link("Edit Discount")
+
+    expect(current_path).to eq("/merchants/#{@merch_1.id}/discounts/#{@discount_1.id}/edit")
+  end
+
 end
