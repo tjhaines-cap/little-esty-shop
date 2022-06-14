6c1d46e019624c42046af83b56a93582f00dea6e
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-14 09:26:16 -0600

    Ref: filter by merchant in total and discounted revenue on merchant invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -32,7 +32,7 @@ class Invoice < ApplicationRecord
     sum = 0
     if bulk_discounts != []
       invoice_items.each do |invoice_item|
-        discount = invoice_item.discount[0]
+        discount = invoice_item.discount_applied[0]
         if discount
           sum += (invoice_item.quantity * invoice_item.unit_price) * ((100 - discount.percentage) / 100.0)
         else
@@ -51,7 +51,7 @@ class Invoice < ApplicationRecord
     sum = 0
     if bulk_discounts != []
       invoice_items.where(item_id: Merchant.find(merchant_id).items.ids).each do |invoice_item|
-        discount = invoice_item.discount[0]
+        discount = invoice_item.discount_applied[0]
         if discount
           sum += (invoice_item.quantity * invoice_item.unit_price) * ((100 - discount.percentage) / 100.0)
         else
@@ -60,6 +60,13 @@ class Invoice < ApplicationRecord
       end
     end
     return sum
+    # bulk_discounts.where(merchant_id: merchant_id).where("invoice_items.quantity >= bulk_discounts.quantity").order(percentage: :desc).limit(1)
+    # wip = invoice_items.where(item_id: Merchant.find(merchant_id).items.ids)
+    # binding.pry
+    # .sum("(quantity * unit_price) * ((100 - #{invoice_items.discount_applied}) / 100.0)")
+    # .sum("(quantity * unit_price) * ((100 - #{bulk_discounts.where(merchant_id: merchant_id).where("invoice_items.quantity >= bulk_discounts.quantity").order(percentage: :desc).limit(1)[0].percentage}) / 100.0)")
+    # .sum("(quantity * unit_price) * ((100 - invoice_items.discount) / 100)")
+    # binding.pry
   end
 
 
